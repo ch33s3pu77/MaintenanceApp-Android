30bb033b732863e4ddc698c451616affb19fa109
Stewart Rutledge <stewart.rut@gmail.com>
2020-05-14 22:17:00 +0100

    In Progress layout

---
+++ app/src/main/java/com/demo/maintenanceapp/output/InProgress.java
@@ -0,0 +1,16 @@
+package com.demo.maintenanceapp.output;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.demo.maintenanceapp.R;
+
+public class InProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_progress);
+    }
+}
