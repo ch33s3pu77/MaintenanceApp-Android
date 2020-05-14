c169649afbf3603037ee610303cdee0c34550a84
Stewart Rutledge <stewart.rut@gmail.com>
2020-05-14 20:01:32 +0100

    Job order layout

---
+++ app/src/main/java/com/demo/maintenanceapp/input/PutOnHold.java
@@ -0,0 +1,16 @@
+package com.demo.maintenanceapp.input;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.demo.maintenanceapp.R;
+
+public class PutOnHold extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_put_on_hold);
+    }
+}
