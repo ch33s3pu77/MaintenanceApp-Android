7fe1aac88de84ee73a39f9f33243cd35193320e8
Stewart Rutledge <stewart.rut@gmail.com>
2020-05-15 13:44:18 +0100

    Dashboard functionality

--- app/src/main/java/com/demo/maintenanceapp/Dashboard.java
+++ app/src/main/java/com/demo/maintenanceapp/Dashboard.java
@@ -2,13 +2,77 @@ package com.demo.maintenanceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.demo.maintenanceapp.output.Completed;
+import com.demo.maintenanceapp.output.InProgress;
+import com.demo.maintenanceapp.output.MyJobs;
+import com.demo.maintenanceapp.output.NewJobs;
+import com.demo.maintenanceapp.output.NotStarted;
+import com.demo.maintenanceapp.output.OnHold;
 
 public class Dashboard extends AppCompatActivity {
+    Button btnMyJobs, btnCompleted, btnOnHold, btnInProgress, btnNotStarted, btnNewJobs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        btnMyJobs = findViewById(R.id.btn_MyJobs);
+        btnMyJobs.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), MyJobs.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCompleted = findViewById(R.id.btn_Complete);
+        btnCompleted.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), Completed.class);
+                startActivity(intent);
+            }
+        });
+
+        btnOnHold = findViewById(R.id.btn_OnHold);
+        btnOnHold.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), OnHold.class);
+                startActivity(intent);
+            }
+        });
+
+        btnInProgress = findViewById(R.id.btn_InProgress);
+        btnInProgress.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), InProgress.class);
+                startActivity(intent);
+            }
+        });
+
+        btnNotStarted = findViewById(R.id.btn_NotStarted);
+        btnNotStarted.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), NotStarted.class);
+                startActivity(intent);
+            }
+        });
+
+        btnNewJobs = findViewById(R.id.btn_NewJobs);
+        btnNewJobs.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), NewJobs.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/demo/maintenanceapp/output/NewJobs.java
@@ -0,0 +1,16 @@
+package com.demo.maintenanceapp.output;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.demo.maintenanceapp.R;
+
+public class NewJobs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_jobs);
+    }
+}
