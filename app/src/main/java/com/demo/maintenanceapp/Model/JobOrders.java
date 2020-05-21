dd03a17e249c732e75ae32f36b2821a708c4ef80
Stewart Rutledge <stewart.rut@gmail.com>
2020-05-21 16:51:12 +0100

    Job orders object class

---
+++ app/src/main/java/com/demo/maintenanceapp/Model/JobOrders.java
@@ -0,0 +1,33 @@
+package com.demo.maintenanceapp.Model;
+
+public class JobOrders {
+    String mJobId, mType, mPriority, mStatus, mLocation;
+
+    public JobOrders(String mJobId, String mType, String mPriority, String mStatus, String mLocation) {
+        this.mJobId = mJobId;
+        this.mType = mType;
+        this.mPriority = mPriority;
+        this.mStatus = mStatus;
+        this.mLocation = mLocation;
+    }
+
+    public String getmJobId() {
+        return mJobId;
+    }
+
+    public String getmType() {
+        return mType;
+    }
+
+    public String getmPriority() {
+        return mPriority;
+    }
+
+    public String getmStatus() {
+        return mStatus;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+}
