8004fd66e58962cbd70c5acc7f0e75587af6cc61
Stewart Rutledge <stewart.rut@gmail.com>
2020-05-22 13:10:57 +0100

    Status adapter functionality

---
+++ app/src/main/java/com/demo/maintenanceapp/Package/StatusAdapter.java
@@ -0,0 +1,43 @@
+package com.demo.maintenanceapp.Package;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.demo.maintenanceapp.Model.JobOrders;
+import com.demo.maintenanceapp.R;
+
+import java.util.List;
+
+public class StatusAdapter extends ArrayAdapter<JobOrders> {
+    private List<JobOrders> jobOrdersList;
+    private Context context;
+
+    public StatusAdapter(List<JobOrders> jobOrdersList, Context context){
+        super(context, R.layout.list_status_jobs, jobOrdersList);
+        this.jobOrdersList = jobOrdersList;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflator = LayoutInflater.from(context);
+        View view = inflator.inflate(R.layout.list_status_jobs, null, true);
+
+        TextView jobId = (TextView) view.findViewById(R.id.txt_ID);
+        TextView type = (TextView) view.findViewById(R.id.txt_Type);
+        TextView priority = (TextView) view.findViewById(R.id.txt_Priority);
+        TextView location = (TextView) view.findViewById(R.id.txt_Location);
+
+        JobOrders jobOrder = jobOrdersList.get(position);
+        jobId.setText(jobOrder.getmJobId());
+        type.setText(jobOrder.getmType());
+        priority.setText(jobOrder.getmPriority());
+        location.setText(jobOrder.getmLocation());
+
+        return view;
+    }
+}
